4b161550c413f387882c476652a382cb47db5e92
samolia <samolia9@gmail.com>
2022-10-22 00:32:45 +0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.posts_list, name='my_posts'),
     path('post/<int:pk>/', views.post_details, name='post_details'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+
